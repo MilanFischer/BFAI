66ef962df754d55abdea79239aa6f3b0362a5bbe
MilanFischer <fischer.milan@gmail.com>
2025-04-16 16:05:02 +0200

    initial commit

---
+++ src/colors.R
@@ -0,0 +1,27 @@
+# Create a custom color palette 
+custom_colors_country <- c(
+  "AUT" = "#457B9D",  # Austria - cooler
+  "BGR" = "#F4A261",  # Bulgaria - warmer
+  "CZE" = "#2A9D8F",  # Czech Republic - neutral
+  "ESP" = "#E63946",  # Spain - warm red
+  "FRA" = "#E76F51",  # France - warm orange
+  "GRC" = "#F4A261",  # Greece - warm
+  "HRV" = "#E9C46A",  # Croatia - warm yellow
+  "ITA" = "#F4A261",  # Italy - warm orange
+  "POL" = "#457B9D",  # Poland - cool blue
+  "PRT" = "#7E3F98",  # Portugal - deep purple
+  "SVN" = "#2A9D8F",  # Slovenia - neutral
+  "SWE" = "#1D3557"   # Sweden - cool dark blue
+)
+
+# Create a custom color palette with a reference line entry
+custom_colors_GCM <- c(
+  "Baseline" = "#E63946",
+  "cmcc-esm2" = "#1D3557",
+  "ec-earth3" = "#457B9D",
+  "gfdl-esm4" = "#2A9D8F",
+  "mpi-esm1-2-hr" = "#F4A261",
+  "mri-esm2-0" = "#E9C46A",
+  "taiesm1" = "#A8DADC",
+  "Reference" = "#2b2b2b"  # Color for the reference line
+)
---
+++ src/corellation_filter.R
@@ -0,0 +1,37 @@
+corr_filter <- function(data, cutoff = 0.9){
+  #-------------------------------------------------------------------------------
+  # Step 1: Remove Highly Correlated Features (keeping the one most correlated with log_BFA1000)
+  cor_matrix <- cor(
+    data |> select(-all_of(c(target, "Country", "Year"))),
+    use = "pairwise.complete.obs"
+  )
+  
+  correlation_with_target <- cor(
+    data |> select(-all_of(c(target, "Country", "Year"))), 
+    data |> pull(all_of(target)), 
+    use = "pairwise.complete.obs"
+  )
+  
+  high_corr <- findCorrelation(cor_matrix, cutoff = cutoff)
+  high_corr_pairs <- combn(names(data)[-1], 2, simplify = FALSE)
+  remove_vars <- numeric()
+  
+  # Identify highly correlated pairs and keep the one with stronger correlation with log_BFA1000
+  for (i in 1:(ncol(cor_matrix) - 1)) {
+    for (j in (i + 1):ncol(cor_matrix)) {
+      if (!is.na(cor_matrix[i, j]) && abs(cor_matrix[i, j]) > cutoff) {
+        var1 <- colnames(cor_matrix)[i]
+        var2 <- colnames(cor_matrix)[j]
+        if (abs(correlation_with_target[var1, 1]) >= abs(correlation_with_target[var2, 1])) {
+          remove_vars <- c(remove_vars, var2)
+        } else {
+          remove_vars <- c(remove_vars, var1)
+        }
+      }
+    }
+  }
+  
+  keep_vars <- setdiff(names(data), unique(remove_vars))
+  
+  return(keep_vars)
+}
